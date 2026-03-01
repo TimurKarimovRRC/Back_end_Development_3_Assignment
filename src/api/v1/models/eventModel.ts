d04ac99759edee1c6605a340645f1f497c601658
Timur Karimov <tkarimov@academic.rrc.ca>
2026-03-01 13:58:16 -0600

    update event  interfaces

--- src/api/v1/models/eventModel.ts
+++ src/api/v1/models/eventModel.ts
@@ -7,3 +7,15 @@ export interface Event {
   createdAt: Date;
   updatedAt: Date;
 }
+
+export interface CreateEventInput {
+  name: string;
+  date: string;
+  capacity: number;
+}
+
+export interface UpdateEventInput {
+  name?: string;
+  date?: string;
+  capacity?: number;
+}

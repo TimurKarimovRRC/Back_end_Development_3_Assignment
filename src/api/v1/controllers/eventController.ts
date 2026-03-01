4e5fd0d0bc4bfeb09ab951c99ad1f421add1b071
Timur Karimov <tkarimov@academic.rrc.ca>
2026-03-01 15:05:50 -0600

    implement create event controller

--- src/api/v1/controllers/eventController.ts
+++ src/api/v1/controllers/eventController.ts
@@ -1,8 +1,18 @@
 import { Request, Response } from "express";
 import { HTTP_STATUS } from "../../../constants/httpConstants";
+import { CreateEventInput, Event } from "../models/eventModel";
+import { createEvent } from "../services/eventService";
 
-export function createEventController(_request: Request, response: Response): void {
-  response.status(HTTP_STATUS.CREATED).json({
-    message: "just for test"
-  });
+export async function createEventController(request: Request, response: Response): Promise<void> {
+  try {
+    const createEventInput: CreateEventInput = request.body as CreateEventInput;
+
+    const createdEvent: Event = await createEvent(createEventInput);
+
+    response.status(HTTP_STATUS.CREATED).json(createdEvent);
+  } catch (_error: unknown) {
+    response.status(HTTP_STATUS.INTERNAL_SERVER_ERROR).json({
+      error: "Failed to create event"
+    });
+  }
 }

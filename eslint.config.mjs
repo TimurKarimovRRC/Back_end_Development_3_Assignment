d2fd72aa9b3894915ac645ad0759ba0aec83a667
Timur Karimov <tkarimov@academic.rrc.ca>
2026-03-02 00:46:22 -0600

    fix

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,7 @@ import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
 import tsParser from "@typescript-eslint/parser";
 
+
 export default tseslint.config(
     {
         ignores: [
